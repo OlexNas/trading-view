480d94ab606f63c71de9d41e5be04e4f7bf10b55
OlexNas <96747321+OlexNas@users.noreply.github.com>
2022-01-03 10:34:29 -0500

    Update pull.py

--- pull.py
+++ pull.py
@@ -2,9 +2,9 @@ from kucoin.client import Client
 from myModels import NewAsset
 
 
-api_key = "6198374f85f69100014e29ad"
-api_secret = "4790218a-61df-43c6-8e34-251ade3291ab"
-passphrase = "Ioipioipoiop133"
+api_key = "#"
+api_secret = "#"
+passphrase = "#"
 
 
 
